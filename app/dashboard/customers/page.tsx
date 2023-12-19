0ce62ee91a148dc2f0fd67388b80576be42a6906
kostiuchenkoo <kostiuchenkoo@upmc.edu>
2023-12-19 10:35:24 +0200

    finished customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Metadata} from "next";
+import {fetchFilteredCustomers} from "../../lib/data";
+import CustomersTable from "../../ui/customers/table";
 
 export const metadata: Metadata = {
     title: 'Customers'
 };
 
-const Page = () => {
+const Page = async ({searchParams,} : {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) => {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
     return (
-        <div>
-            <p>Customers Page</p>
-        </div>
+        <CustomersTable customers={customers}/>
     );
 };
 
